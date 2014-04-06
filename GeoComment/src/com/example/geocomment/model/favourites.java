816410be9f0460b721d44abc53944db0a2ffbf66
telbohtimy <elbohtim@ualberta.ca>
2014-04-06 14:38:47 -0600

    favourites with no save work

--- GeoComment/src/com/example/geocomment/model/favourites.java
+++ GeoComment/src/com/example/geocomment/model/favourites.java
@@ -3,22 +3,23 @@ package com.example.geocomment.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.widget.ArrayAdapter;
+
 import com.example.geocomment.CommentBrowseActivity;
 import com.example.geocomment.GeoCommentActivity;
 
 public class favourites {
 	GeoCommentActivity activity;
 	CommentBrowseActivity activity1;
-	static List<Commentor> favouriteGeo=new ArrayList<Commentor>();;
-
-	static List<Commentor> favouriteBrowse=new ArrayList<Commentor>();;
-
+	static List<Commentor> favouriteGeo=new ArrayList<Commentor>();
+	static List<Commentor> favouriteBrowse=new ArrayList<Commentor>();
+	static List<Commentor> favourites= new ArrayList<Commentor>();
 	
 	public static void updateGeo(List<Commentor> commentList){
 		favouriteGeo.clear();
+		favourites.clear();
 		for(Commentor c: commentList){
 			if(c.isFavourite()==true){
-				if(!favouriteGeo.contains(c));
 					favouriteGeo.add(c);
 			}
 		}
@@ -39,7 +40,7 @@ public class favourites {
 	}
 	
 	public static List<Commentor> returnFav(){
-		List<Commentor> favourites= new ArrayList<Commentor>();
+		favourites.clear();
 		favourites.addAll(favouriteBrowse);
 		favourites.addAll(favouriteGeo);
 		return favourites;
